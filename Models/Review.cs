165d4674a28dd93d70b06ca7b5c3d13c01f67fee
moldyhacker <56985918+MoldyHacker@users.noreply.github.com>
2023-04-21 18:57:36 -0500

    added some temp stars to the category index page

--- Models/Review.cs
+++ Models/Review.cs
@@ -1,10 +1,10 @@
 using System.ComponentModel.DataAnnotations;
-public class Rating
+public class Review
 {
     public int RatingId {get; set;}
     [Required(ErrorMessage ="Review is required")]
     [Range(0,5, ErrorMessage = "Must be 0-5")]
-    public int Review {get; set;}
+    public int Rating {get; set;}
     public string Comment {get; set;}
     public int ProductId {get; set;}
     public Product Product {get; set;}
