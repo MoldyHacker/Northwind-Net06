2fd55e48ae8028afa2199c85d85f5ed650eb7c9b
moldyhacker <56985918+MoldyHacker@users.noreply.github.com>
2023-04-21 19:17:52 -0500

    fixed DataContext and Review

--- Models/DataContext.cs
+++ Models/DataContext.cs
@@ -8,7 +8,7 @@ public class DataContext : DbContext
   public DbSet<Category> Categories { get; set; }
   public DbSet<Discount> Discounts { get; set; }
   public DbSet<Customer> Customers { get; set; }
-  public DbSet<Review> Review { get; set; }
+  public DbSet<Review> Reviews { get; set; }
 
   public void AddCustomer(Customer customer)
   {
--- Models/Review.cs
+++ Models/Review.cs
@@ -6,6 +6,7 @@ public class Review
     [Range(0,5, ErrorMessage = "Must be 0-5")]
     public int Rating {get; set;}
     public string Comment {get; set;}
+    public int CustomerId { get; set; }
     public int ProductId {get; set;}
     public Product Product {get; set;}
 }
