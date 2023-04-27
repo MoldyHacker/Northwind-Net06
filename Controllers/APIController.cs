5e65efe90eb7e7fd43477393dc7f226a5a9a30dc
moldyhacker <56985918+MoldyHacker@users.noreply.github.com>
2023-04-26 20:38:29 -0500

    edit Review

--- Controllers/APIController.cs
+++ Controllers/APIController.cs
@@ -22,7 +22,7 @@ namespace Northwind.Controllers
         public IEnumerable<Product> GetDiscontinued(bool discontinued) => _dataContext.Products.Where(p => p.Discontinued == discontinued).OrderBy(p => p.ProductName);
         [HttpGet, Route("api/category")]
         // returns all categories
-         public IEnumerable<Category> GetCategory() => _dataContext.Categories.Include("Products").OrderBy(c => c.CategoryName);
+        public IEnumerable<Category> GetCategory() => _dataContext.Categories.Include("Products").OrderBy(c => c.CategoryName);
         [HttpGet, Route("api/category/{CategoryId}/product")]
         // returns all products in a specific category
         public IEnumerable<Product> GetByCategory(int CategoryId) => _dataContext.Products.Where(p => p.CategoryId == CategoryId).OrderBy(p => p.ProductName);
