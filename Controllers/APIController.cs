fe5d35675f6c6d8cb7705aa8f96a2e8ce57e6b1b
moldyhacker <56985918+MoldyHacker@users.noreply.github.com>
2023-05-03 22:32:51 -0500

    added a linq statment and added spacing for statments

--- Controllers/APIController.cs
+++ Controllers/APIController.cs
@@ -14,24 +14,43 @@ namespace Northwind.Controllers
         [HttpGet, Route("api/product")]
         // returns all products
         public IEnumerable<Product> Get() => _dataContext.Products.OrderBy(p => p.ProductName);
+        
         [HttpGet, Route("api/product/{id}")]
         // returns specific product
         public Product Get(int id) => _dataContext.Products.FirstOrDefault(p => p.ProductId == id);
+        
         [HttpGet, Route("api/product/discontinued/{discontinued}")]
         // returns all products where discontinued = true/false
         public IEnumerable<Product> GetDiscontinued(bool discontinued) => _dataContext.Products.Where(p => p.Discontinued == discontinued).OrderBy(p => p.ProductName);
+        
         [HttpGet, Route("api/category")]
         // returns all categories
         public IEnumerable<Category> GetCategory() => _dataContext.Categories.Include("Products").OrderBy(c => c.CategoryName);
+        
         [HttpGet, Route("api/category/{CategoryId}/product")]
         // returns all products in a specific category
         public IEnumerable<Product> GetByCategory(int CategoryId) => _dataContext.Products.Where(p => p.CategoryId == CategoryId).OrderBy(p => p.ProductName);
+        
         [HttpGet, Route("api/category/{CategoryId}/product/discontinued/{discontinued}")]
         // returns all products in a specific category where discontinued = true/false
         public IEnumerable<Product> GetByCategoryDiscontinued(int CategoryId, bool discontinued) => _dataContext.Products.Where(p => p.CategoryId == CategoryId && p.Discontinued == discontinued).OrderBy(p => p.ProductName);
+        
         [HttpGet, Route("api/category/{CategoryId}/productwithreviews/discontinued/{discontinued}")]
         // returns all products in a specific category where discontinued = true/false
         public IEnumerable<ProductReview> GetByCategoryDiscontinuedWithRating(int CategoryId, bool discontinued){
+            // return (from product in _dataContext.Products 
+            // join review in _dataContext.Reviews 
+            // on product.ProductId equals review.ProductId
+            // group review by product into g
+            // select new ProductReview 
+            // {
+            //     ProductId = g.Key.ProductId, 
+            //     ProductName = g.Key.ProductName,
+            //     UnitPrice = g.Key.UnitPrice,
+            //     UnitsInStock = g.Key.UnitsInStock,
+            //     AvgRating = (decimal)g.Average(r => r.Rating)
+            // }).ToList();
+
             var products = _dataContext.Products.Include("Reviews").Where(p => p.CategoryId == CategoryId && p.Discontinued == discontinued).OrderBy(p => p.ProductName);
             List<ProductReview> productReviews = new List<ProductReview>();
             foreach(var p in products){
@@ -49,14 +68,11 @@ namespace Northwind.Controllers
             };
             return productReviews;
         }
-        [HttpGet, Route("api/customer/purchases/{CustomerId}")]
-        // returns all orders for a given customer
-        public IEnumerable<Order> GetByOrder(int CustomerId) => _dataContext.Orders.Where(o => o.CustomerId == CustomerId).OrderBy(o => o.OrderId);
-        [HttpGet, Route("api/customer/purchases/purchasedetails/{OrderId}")]
-        // // returns specfic order, ordered by product name
-        public IEnumerable<OrderDetail> GetByOrderDetail(int OrderId) => _dataContext.OrderDetails.Where(o => o.OrderId == OrderId).OrderBy(o => o.ProductId);
+
+        [HttpPost, Route("api/addtocart")]
         // returns all reviews for a specfic product based on a specfic customer
         public IEnumerable<Review> GetProductReview(int ProductId, int CustomerId) => _dataContext.Reviews.Where(r => r.ProductId == ProductId && r.CustomerId == CustomerId).OrderBy(r => r.Product.ProductName);
+        
         [HttpPost, Route("api/addtocart")]
         // adds a row to the cartitem table
         public CartItem Post([FromBody] CartItemJSON cartItem) => _dataContext.AddToCart(cartItem);
