8ff8e479a594ec7c0ba2dcc98839fe00e96bdc46
moldyhacker <56985918+MoldyHacker@users.noreply.github.com>
2023-05-15 21:07:02 -0500

    updated stuff

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -2,5 +2,8 @@ using Microsoft.AspNetCore.Identity;
 
 public class AppUser : IdentityUser
 {
-
+    public static implicit operator IdentityResult(AppUser v)
+    {
+        throw new NotImplementedException();
+    }
 }
